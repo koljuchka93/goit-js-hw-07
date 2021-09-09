fd1071ec079c3db893e5e7266388c4c7e203a37d
Kateryna <koljuchka93@gmail.com>
2021-09-10 00:14:20 +0300

    add task 1 and 2

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,6 @@
 const categoriesRef = document.querySelector('#categories')
 console.log(`В списке ${categoriesRef.children.length} категории.`)
+
+const itemRef = document.querySelectorAll('.item h2')
+itemRef.forEach (element => console.log(`Категория: ${element.textContent}
+Количество элементов: ${element.nextElementSibling.children.length}`))
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const ingredientsUlRef = document.querySelector('#ingredients');
+ 
+const liRef = ingredients.map(ingredient => {
+  const ingredientLiRef = document.createElement('li')
+  ingredientLiRef.textContent = ingredient;
+
+  return ingredientLiRef
+});
+
+console.log(liRef)
+
+ingredientsUlRef.append(...liRef)
